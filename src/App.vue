9d261f68904233ec051806793be4a915e4d2fed9
Hermann EHO <hermannleboss@gmail.com>
2023-03-10 19:25:46 +0000

    feat: initialisation

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+  <RouterView />
+</template>
