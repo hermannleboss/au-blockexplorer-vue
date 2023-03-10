9e2c08a784a3f7d4c1b177a97e30cbe3d1ac1880
Hermann EHO <hermannleboss@gmail.com>
2023-03-10 19:37:41 +0000

    feat: add title add remove default font size

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+  <h1>Vue Js BlockExplorer</h1>
   <nav>
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/about">About</RouterLink>
