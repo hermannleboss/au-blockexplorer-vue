94fda334f5435db610f383936d1a5b7d3263b2a9
Hermann EHO <hermannleboss@gmail.com>
2023-03-12 13:03:01 +0000

    feat: Layout style

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import {RouterLink, RouterView} from 'vue-router'
 
 <template>
   <div class="flex justify-center bg-gray-100 min-h-screen">
-    <div class="max-w-5xl" style="min-width: 1400px">
+    <div class="max-w-7xl w-full px-5">
       <div class="flex justify-between">
 
         <h1 class="text-2xl font-bold my-4">Vue Js BlockExplorer</h1>
