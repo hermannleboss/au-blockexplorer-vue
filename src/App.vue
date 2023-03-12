2fbff3cc1195c5748825512e79a08e5d2ea84900
Hermann EHO <hermannleboss@gmail.com>
2023-03-12 14:51:59 +0000

    refactor: update template structure

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,27 @@ import {RouterLink, RouterView} from 'vue-router'
 
 <template>
   <div class="flex justify-center bg-gray-100 min-h-screen">
-    <div class="max-w-7xl w-full px-5">
+    <div class="max-w-7xl w-full px-5 min-h-full flex flex-col justify-between">
+      <div>
+        <div class="flex justify-between">
+          <p class="text-2xl font-bold my-4">Vue Js BlockExplorer</p>
+          <nav class="flex  items-center gap-5 ">
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+          </nav>
+        </div>
+        <RouterView/>
+      </div>
       <div class="flex justify-between">
-
-        <h1 class="text-2xl font-bold my-4">Vue Js BlockExplorer</h1>
+        <p class="text-2xl font-bold my-4">Vue Js BlockExplorer</p>
+        <div class="flex  items-center gap-5 ">
+          Author <RouterLink to="https://www.hermanneho.com/" class="text-blue-700 hover:text-blue-300">Hermann EHO</RouterLink>
+        </div>
         <nav class="flex  items-center gap-5 ">
           <RouterLink to="/">Home</RouterLink>
           <RouterLink to="/about">About</RouterLink>
         </nav>
       </div>
-      <RouterView/>
     </div>
   </div>
 </template>
